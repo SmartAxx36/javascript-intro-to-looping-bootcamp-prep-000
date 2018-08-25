2df4883c80c9f5f81152d714cf3beddb6aa80675
Gavin Moore <gavizoid16@gmail.com>
2018-08-25 13:10:29 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+var array = []
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+    console.log(array.push("I am 1 strange loop."))
+  } else {
+    array.push("I am ${i} strange loops.")
+return array
+}}}
+function doWhileLoop(array) {
+  do array.shift()
+  while (array.length > 0)
+return array
+}
